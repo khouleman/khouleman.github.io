705ffaeadda6039cb0207b7472432373079b9b59
Nathan DEMURU <blakei.57@icloud.com>
2022-03-22 15:13:28 +0100

    adding dist subtree

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/CV_Developpeur_Web_Nathan_DEMURU/" : "/",
+    publicPath: '/src/App.vue',
+    assetsDir: 'src/assets'
 };
